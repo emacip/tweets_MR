605ec58e526fb4493befe3646ef2633ee4442999
emacip <emacipbelmonte@gmail.com>
2020-01-08 20:21:07 +0000

    mapreduce para ejecutar en local

--- tweet_reduce.py
+++ tweet_reduce.py
@@ -1,14 +1,27 @@
 import sys
+from collections import Counter
 
 dic = {}
+with open('reduce', 'r') as file:
+    for line in file:
+        location, value = line.split('\t')
+        #value = int(value)
 
-for line in sys.stdin:
-    location, value = line.split('\t')
-    value = int(value)
-    if location in dic:
-        dic[location] += value
-    else:
-        dic[location] = value
+        value = value.split('/')
+        value[1] = value[1].replace('\n', '')
+
+        if location in dic:
+            dic[location]['score'] += int(value[0])
+            if value[1] != "empty":
+               dic[location]['hashtag'].append(value[1])
+        else:
+            dic[location] = {}
+
+            dic[location]['score'] = int(value[0])
+            dic[location]['hashtag'] = []
+            if value[1] != "empty":
+                dic[location]['hashtag'].append(value[1])
 
 for key in dic.keys():
-    print('{0}\t{1}'.format(key, dic[key]))
+    res = [key for key, value in Counter(dic[key]['hashtag']).most_common()]
+    print('{0}\t{1}'.format(key, str(dic[key]['score'])+" "+ str(res[:10])))
